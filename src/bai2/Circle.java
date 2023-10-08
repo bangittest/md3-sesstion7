85c07a5dac3c352c826c5a6471a836bbc6db5b0c
bangnguyen100501 <banghanam01@gmail.com>
2023-10-09 01:10:24 +0700

    up

---
+++ src/bai2/Circle.java
@@ -0,0 +1,24 @@
+package bai2;
+
+class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+        color = "red";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ src/baitap/StaticMethod.java
@@ -0,0 +1,11 @@
+package baitap;
+
+public class StaticMethod {
+    private static double PI=3.14;
+
+    public static double calCircleArea(double radius) {
+        return PI*radius*radius;
+    }
+
+
+}
